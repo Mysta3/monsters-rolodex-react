e9195480f1304a778d83938da7ea3733cc087ba8
Mysta3 <myshawnestallings@gmail.com>
2022-06-18 16:36:07 -0700

    created new card component and moved styling

--- src/components/card-list/cardList.component.jsx
+++ src/components/card-list/cardList.component.jsx
@@ -1,13 +1,22 @@
 import { Component } from 'react';
+import Card from '../card/card.component.jsx';
+import './cardList.styles.css';
 
 class CardList extends Component {
   render(){
     const { monsters } = this.props;
    
       return (
-        <div>
-          {monsters.map((monster, index) =>  {
-           return <h1 key={index}>{monster.name}</h1>
+        <div className='card-list'>
+          {monsters.map((monster) =>  {
+            const {name, email, id} = monster;
+           return (
+            <Card 
+              name={name}
+              email={email}
+              id={id}
+            />
+            )
           })}
         </div>
       );
