de216be99587df017034f8b05e6e72850e8c6d84
Mysta3 <myshawnestallings@gmail.com>
2022-06-21 15:29:48 -0700

    migrated to Reactv18

--- src/components/card-list/cardList.component.jsx
+++ src/components/card-list/cardList.component.jsx
@@ -2,11 +2,13 @@ import Card from '../card/card.component.jsx';
 import './cardList.styles.css';
 
 const CardList = ({ monsters }) => {
+ return (
   <div className='card-list'>
     {monsters.map((monster) =>  {
-      return <Card monster={monster}/>
+      return <Card key={monster.id} monster={monster} />
     })};
     </div>
+    );
 }
 
 export default CardList;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,9 @@
 import './card.styles.css';
-const Card = ({name, email, id}) =>  {
+const Card = ({ monster }) =>  {
+  const { id, name, email } = monster;
   return (
-    <div className='card-container' key={id}>
-      <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`}  />
+    <div className='card-container'>
+      <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
